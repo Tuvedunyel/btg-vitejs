aa521f6fa9a5ccef4464e0458bf893fbd53c311a
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-11-03 13:57:39 +0100

    mise en place de la navigation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script>
+  export default {
+    name: 'BTG Communication',
+  }
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router/index.js";
+import App from "./App.vue";
+import Style from "./sass/style.scss";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
